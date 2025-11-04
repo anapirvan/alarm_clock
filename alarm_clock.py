0b4d30acf46490e78060f7f8cd888e94dba1c492
ana.pirvan <anampirvan05@gmail.com>
2025-11-04 19:19:23 +0200

    first commit

---
+++ alarm_clock.py
@@ -0,0 +1,32 @@
+import time
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file="my_music.mp3"
+
+    while True:
+        current_time=datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        
+        if current_time==alarm_time:
+            print("WAKE UP! 😴")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+
+            break
+
+        time.sleep(1)
+
+
+def main():
+    alarm_time=input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
+
+main()
